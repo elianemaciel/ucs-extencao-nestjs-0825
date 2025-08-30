d6cbafbfb6312f150e4b18a182b043aa4dd563f5
Eliane Isadora Faveron Maciel <eliane.faveron@gmail.com>
2025-08-30 12:53:37 +0000

    ajustes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('login')
+    @ApiBody({ schema: {
+        type: 'object',
+        properties: {
+        userId: { type: 'string', example: '12345' },
+        },
+        required: ['userId'],
+    }, })
     login(@Body() body: { userId: string }) {
         const token = this.authService.generateToken(body.userId);
         return { accessToken: token };
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [ConfigModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, AuthGuard],
+  exports: [AuthGuard, AuthService]
 })
 export class AuthModule {}
